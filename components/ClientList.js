0080d941f39c6eb7b1c6930f04113f71ce6f91ee
mycomport1 <111806665+mycomport1@users.noreply.github.com>
2024-08-14 15:18:13 +0200

    optimizing

--- components/ClientList.js
+++ components/ClientList.js
@@ -1,16 +1,20 @@
-import React, { useEffect, useState } from 'react';
+// ClientsContext.js
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const ClientsContext = createContext();
 
-function ClientList() {
+export const useClients = () => useContext(ClientsContext);
+
+export const ClientsProvider = ({ children }) => {
     const [clients, setClients] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        async function fetchClients() {
+        const fetchClients = async () => {
             setIsLoading(true);
             try {
+                const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
                 const response = await axios.get(`${BACKEND_URL}/clients`);
                 setClients(response.data);
             } catch (error) {
@@ -18,10 +22,25 @@ function ClientList() {
             } finally {
                 setIsLoading(false);
             }
-        }
-        
+        };
         fetchClients();
     }, []);
+
+    return (
+        <ClientsContext.Provider value={{ clients, isLoading }}>
+            {children}
+        </ClientsContext.Provider>
+    );
+};
+```
+
+```jsx
+// ClientList.js
+import React from 'react';
+import { useClients } from './ClientsContext';
+
+function ClientList() {
+    const { clients, isLoading } = useClients();
 
     return (
         <div>
@@ -42,3 +61,22 @@ function ClientList() {
 }
 
 export default ClientList;
+```
+
+```jsx
+// App.js or similar
+import React from 'react';
+import { ClientsProvider } from './ClientsContext';
+import ClientList from './ClientList';
+
+function App() {
+  return (
+    <ClientsProvider>
+      <div className="App">
+        <ClientList />
+      </div>
+    </ClientsProvider>
+  );
+}
+
+export default App;
